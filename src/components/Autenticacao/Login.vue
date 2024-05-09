97792d1c1db4a2e2d262c8d40fed26264fd242cc
guivpn <guilhermeviniciusgv42@gmail.com>
2024-05-08 23:24:53 -0300

    Atualização

--- src/components/Autenticacao/Login.vue
+++ src/components/Autenticacao/Login.vue
@@ -3,25 +3,28 @@
 
 		<div class="caixa-red">
 			<div class="logo">
-				<img src="https://s3-alpha-sig.figma.com/img/8501/1dd5/a73ae3937112b166e926ef385c33c1b2?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=awZhkgqCwmLIUmOFDGAzPkBaB5MIBxUgbus2QYIIo6vIMl~cKy1h~SqWiHELbNZgGfT-~twlChRKHKLo9vqb0-kpKa5zcr3lDcHVaGT~DsmI52yp3AHXS483vX4Y1vbi2iynAlLIsrW4-SDykLfI~tAXfbUb410RIOFhjjlESWfcNfETzGSo1FdoMAo3feyCTtG5o~f~5KxeWUMZHO0sPiEHPVTZnDfCK3OhRWxySuQbe6cQNLEA3FzxHiqaX1WPtngzRF6WtyJT3ErLZkTPFUjCda4fEmgav9iB7kILtFLbW7Quko4u~9yPKf0gGSbm2DT~rDw7mPg2XGOSzE4E4Q__"
+				<img
+					src="https://s3-alpha-sig.figma.com/img/8501/1dd5/a73ae3937112b166e926ef385c33c1b2?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=awZhkgqCwmLIUmOFDGAzPkBaB5MIBxUgbus2QYIIo6vIMl~cKy1h~SqWiHELbNZgGfT-~twlChRKHKLo9vqb0-kpKa5zcr3lDcHVaGT~DsmI52yp3AHXS483vX4Y1vbi2iynAlLIsrW4-SDykLfI~tAXfbUb410RIOFhjjlESWfcNfETzGSo1FdoMAo3feyCTtG5o~f~5KxeWUMZHO0sPiEHPVTZnDfCK3OhRWxySuQbe6cQNLEA3FzxHiqaX1WPtngzRF6WtyJT3ErLZkTPFUjCda4fEmgav9iB7kILtFLbW7Quko4u~9yPKf0gGSbm2DT~rDw7mPg2XGOSzE4E4Q__"
 					alt="Logo do projeto">
 			</div>
 			<div class="caixa-escura">
 				<div class="caixa-para-botoes">
 
 					<b-button key="botaoEmail" :variant="buttonVariant01" pill @click="changeColor(1)">E-mail</b-button>
-					<b-button key="botaoCelular" :variant="buttonVariant02" pill
-						@click="changeColor(2)">Celular</b-button>
+					<b-button key="botaoCelular" :variant="buttonVariant02" pill @click="changeColor(2)">Celular</b-button>
 
 				</div>
 
+				<!-- Div principal caso selecionado os botões "email" ou "celular" -->
 				<b-container class="tudao">
-					<b-row iv v-if="buttonVariant01 === 'danger'" class="informacoes-email">
-						<b-row class="texto-h1">
-							<h1>Cadastrar conta com o meu email</h1>
-						</b-row>
 
-						<b-col id="inputs-container">
+					<!-- Condição caso clique no email -->
+					<div v-if="buttonVariant01 === 'danger'" class="informacoes-email">
+						<div>
+							<h1 class="texto-h1">Cadastrar conta com o meu email</h1>
+						</div>
+
+						<div id="inputs-container">
 							<b-col role="group">
 								<label for="input-live"></label>
 								<b-form-input id="input-live" v-model="name" :state="nameState"
@@ -39,38 +42,39 @@
 									aria-describedby="input-live-help input-live-feedback" placeholder="Inserir senha"
 									trim></b-form-input>
 							</b-col>
-						</b-col>
+						</div>
 
+						<!-- botãoLogin -->
+						<b-container class="login">
+							<div>
+									<b-button variant="light">Entrar</b-button>
+							</div>
+						</b-container>
 
+						<!-- botão criar conta -->
 						<b-container class="criar-conta">
-							<b-row>
-								<b-col></b-col>
-								<b-col cols="12">
+							<div>
 									<b-button variant="light">Criar conta</b-button>
-								</b-col>
-								<b-col></b-col>
-							</b-row>
+							</div>
 						</b-container>
 
-						<b-container class="entrar-google">
-							<b-row>
-								<b-col></b-col>
-								<b-col cols="12">
-									<b-button variant="light">Entrar com Google</b-button>
-								</b-col>
-								<b-col></b-col>
-							</b-row>
+						<!-- botão entrar com google -->
+						<b-container >
+							<div>
+									<b-button variant="light" class="botao-padrao">Entrar com Google</b-button>
+							</div>
 						</b-container>
 
-					</b-row>
+					</div>
 
 
-					<b-row v-if="buttonVariant02 === 'danger'" class="informacoes-celular">
-						<b-row cols="1">
-							<h1>Cadastrar conta com o meu email</h1>
-						</b-row>
+					<!-- Condição caso clique no celular -->
+					<div v-if="buttonVariant02 === 'danger'" class="informacoes-celular">
+						<div>
+							<h1 class="texto-h1">Cadastrar conta com o meu celular</h1>
+						</div>
 
-						<b-col id="inputs-container">
+						<div id="inputs-container">
 							<b-col role="group">
 								<label for="input-live"></label>
 								<b-form-input id="input-live" v-model="name" :state="nameState"
@@ -88,30 +92,30 @@
 									aria-describedby="input-live-help input-live-feedback" placeholder="Inserir senha"
 									trim></b-form-input>
 							</b-col>
-						</b-col>
+						</div>
 
+						<!-- botãoLogin -->
+						<b-container class="login">
+							<div>
+									<b-button variant="light">Entrar</b-button>
+							</div>
+						</b-container>
 
+						<!-- botão criar conta -->
 						<b-container class="criar-conta">
-							<b-row>
-								<b-col></b-col>
-								<b-col cols="12">
+							<div>
 									<b-button variant="light">Criar conta</b-button>
-								</b-col>
-								<b-col></b-col>
-							</b-row>
+							</div>
 						</b-container>
 
-						<b-container class="entrar-google">
-							<b-row>
-								<b-col></b-col>
-								<b-col cols="12">
-									<b-button variant="light">Entrar com Google</b-button>
-								</b-col>
-								<b-col></b-col>
-							</b-row>
+						<!-- botão entrar com google -->
+						<b-container>
+							<div>
+									<b-button variant="light" class="botao-padrao">Entrar com Google</b-button>
+							</div>
 						</b-container>
 
-					</b-row>
+					</div>
 				</b-container>
 			</div>
 		</div>
@@ -205,7 +209,6 @@ img {
 button {
 	border-radius: 10px;
 	padding: 10px;
-	/* Adicionei "px" aqui */
 	width: 48%;
 	height: 100%;
 	border: none;
@@ -213,39 +216,43 @@ button {
 }
 
 .tudao {
-	/* margin-bottom: top; */
 	display: flex;
-	/* background-color: yellow; */
 	justify-content: center;
 }
 
 .informacoes-email {
-	margin-top: 10px;
+	margin-top: 50px;
 	width: 50%;
 	height: 90%;
-	background-color: violet;
+	/* background-color: violet; */
 }
 
 .informacoes-celular {
-	margin-top: 10px;
+	margin-top: 50px;
 	width: 50%;
 	height: 90%;
-	background-color: violet;
-	display: flex;
+	/* background-color: violet; */
 }
 
 .texto-h1 {
-	font-size: 5px !important;
+	font-size: 24px;
 	color: white;
 }
 
 #inputs-container {
 	margin-bottom: 20px;
-	background-color: blue;
+	/* background-color: blue; */
 }
 
-.entrar-google {
-	margin-top: 10px;
+.botao-padrao {
+	padding: 10px 20px;
+	font-size: 100px;
+	margin-top: 5px;
 	margin-bottom: 20px;
 }
+
+.criar-conta {
+	margin-top: 5px
+}
+
 </style>
